fa5d04accef7e369159be793ec665a8e0a14731c
penguinspizza <72650078+penguinspizza@users.noreply.github.com>
2022-02-16 04:31:28 +0900

    Completed
    一応完成

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// IDにリンクしたときの位置をナビバーの高さ分だけ下にずらす
-$(function () {
-  $('a[href^="#"]').click(function () {
-    var adjust = -78;
-    var speed = 0;
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top + adjust;
-    $("body,html").animate({ scrollTop: position }, speed, "swing");
-    return false;
-  });
-});
-
 // ナビバー押したら閉じる
 $(".navbar-nav>.close-item>a , .dropdown-menu>li>a").on("click", function () {
   if (this.id != "navbarDropdown") {
