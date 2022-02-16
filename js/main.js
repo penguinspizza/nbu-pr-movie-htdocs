011e58174ce37a9924d74b37cdfc7eaac59b69b2
penguinspizza <72650078+penguinspizza@users.noreply.github.com>
2022-02-16 13:09:03 +0900

    Fix
    別ページへのidリンクでずれる問題をjQueryで解消
    ページタイトルを変更

--- js/main.js
+++ js/main.js
@@ -16,3 +16,19 @@ $('.dropdown').on('show.bs.dropdown', function () {
 $('.dropdown').on('hide.bs.dropdown', function () {
   $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
 });
+
+// 別ページidリンク位置合わせ
+$(function () {
+  // 別ページの場合は以下
+  var urlHash = location.hash;
+  if (urlHash) {
+    $('body,html').stop().scrollTop(0);
+    setTimeout(function () {
+      // ヘッダー固定の場合はヘッダーの高さを数値で入れる、固定でない場合は0
+      var headerHight = 0;
+      var target = $(urlHash);
+      var position = target.offset().top - headerHight;
+      $('body,html').stop().animate({ scrollTop: position }, 400);
+    }, 100);
+  }
+});
